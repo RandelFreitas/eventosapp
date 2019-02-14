bc45abb3ff80923486f7a11971844679108a2bd4
randel <marcosrandelfb@hotmail.com>
2019-02-14 14:51:12 -0300

    Padrao MVC criado. closes #1

--- src/main/java/com/eventoapp/EventosappApplication.java
+++ src/main/java/com/eventoapp/EventosappApplication.java
@@ -1,4 +1,4 @@
-package com.eventoapp.eventosapp;
+package com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/eventoapp/indexController.java
+++ src/main/java/com/eventoapp/indexController.java
@@ -1,4 +1,4 @@
-package com.eventoapp.eventosapp;
+package com.eventoapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
